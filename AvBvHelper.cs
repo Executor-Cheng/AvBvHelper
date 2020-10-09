9ce936235a0975f249e2ad7554b3e25cb7993a32
Executor <847529602@qq.com>
2020-10-09 22:30:37 +0800

    将Table移至rodata段存储。 优化代码性能。 Av2Bv: 53.03ns=>24.71ns(-53.40%) Bv2Av: 30.72ns=>28.96ns(-5.73%)

--- AvBvHelper.cs
+++ AvBvHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace BilibiliUtility
 {
@@ -8,7 +9,7 @@ namespace BilibiliUtility
 
         private const long Add = 8728348608;
 
-        private static readonly byte[] Table = { 102, 90, 111, 100, 82, 57, 88, 81, 68, 83, 85, 109, 50, 49, 121, 67, 107, 114, 54,
+        private static ReadOnlySpan<byte> Table => new byte[] { 102, 90, 111, 100, 82, 57, 88, 81, 68, 83, 85, 109, 50, 49, 121, 67, 107, 114, 54,
                                                  122, 66, 113, 105, 118, 101, 89, 97, 104, 56, 98, 116, 52, 120, 115, 87, 112, 72, 110,
                                                  74, 69, 55, 106, 76, 53, 86, 71, 51, 103, 117, 77, 84, 75, 78, 80, 65, 119, 99, 70 };
 
@@ -18,30 +19,24 @@ namespace BilibiliUtility
             {
                 byte* result = stackalloc byte[12] { 66, 86, 49, 0, 0, 52, 0, 49, 0, 55, 0, 0 };
                 av = (av ^ Xor) + Add;
-                result[11] = table[av / 1 % 58];
+                result[11] = table[av % 58];
                 result[10] = table[av / 58 % 58];
                 result[3] = table[av / 3364 % 58];
                 result[8] = table[av / 195112 % 58];
                 result[4] = table[av / 11316496 % 58];
                 result[6] = table[av / 656356768 % 58];
-                return new string((sbyte*)result, 0, 12);
+                return Encoding.ASCII.GetString(result, 12);
             }
         }
 
-        public static unsafe int Bv2Av(string bv)
+        public static int Bv2Av(string bv)
         {
-            fixed (byte* b = Table)
-            {
-                Span<byte> table = new Span<byte>(b, 58);
-                long sum = 0;
-                sum += table.IndexOf((byte)bv[11]);
-                sum += table.IndexOf((byte)bv[10]) * 58;
-                sum += table.IndexOf((byte)bv[3]) * 3364;
-                sum += table.IndexOf((byte)bv[8]) * 195112;
-                sum += table.IndexOf((byte)bv[4]) * 11316496;
-                sum += table.IndexOf((byte)bv[6]) * 656356768;
-                return (int)((sum - Add) ^ Xor);
-            }
+            return (int)((Table.IndexOf((byte)bv[11]) +
+                          Table.IndexOf((byte)bv[10]) * 58 +
+                          Table.IndexOf((byte)bv[3]) * 3364 +
+                          Table.IndexOf((byte)bv[8]) * 195112 +
+                          Table.IndexOf((byte)bv[4]) * 11316496 +
+                          Table.IndexOf((byte)bv[6]) * 656356768 - Add) ^ Xor);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 B站Av号和Bv号互转
 
 跑分结果:  
-BenchmarkDotNet=v0.12.0, OS=Windows 10.0.18363  
-Intel Core i7-8750H CPU 2.20GHz (Coffee Lake), 1 CPU, 12 logical and 6 physical cores  
-.NET Core SDK=3.1.201  
-  [Host]     : .NET Core 3.1.3 (CoreCLR 4.700.20.11803, CoreFX 4.700.20.12001), X64 RyuJIT  
-  DefaultJob : .NET Core 3.1.3 (CoreCLR 4.700.20.11803, CoreFX 4.700.20.12001), X64 RyuJIT  
+BenchmarkDotNet=v0.12.1, OS=Windows 10.0.17763.678 (1809/October2018Update/Redstone5)
+Intel Core i7-8700K CPU 4.29GHz (Coffee Lake), 1 CPU, 12 logical and 6 physical cores
+.NET Core SDK=5.0.100-rc.1.20452.10
+  [Host]     : .NET Core 5.0.0 (CoreCLR 5.0.20.45114, CoreFX 5.0.20.45114), X64 RyuJIT
+  DefaultJob : .NET Core 5.0.0 (CoreCLR 5.0.20.45114, CoreFX 5.0.20.45114), X64 RyuJIT
 
-
-| Method |        av |           bv |      Mean |    Error |   StdDev |
-|------- |---------- |------------- |----------:|---------:|---------:|
-|  **Av2Bv** | **582921863** |            **?** | **114.96 ns** | **0.474 ns** | **0.444 ns** |
-|  **Bv2Av** |         **?** | **BV1L64y1M7Dp** |  **57.64 ns** | **0.198 ns** | **0.176 ns** |
+```
+| Method |        av |           bv |     Mean |    Error |   StdDev |
+|------- |---------- |------------- |---------:|---------:|---------:|
+|  **Av2Bv** | **582921863** |            **?** | **24.71 ns** | **0.547 ns** | **0.608 ns** |
+|  **Bv2Av** |         **?** | **BV1L64y1M7Dp** | **28.96 ns** | **0.595 ns** | **0.685 ns** |
